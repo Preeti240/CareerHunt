919ca32deda702b207500aa5f9447a95e98fd3d5
Preeti240 <63953096+Preeti240@users.noreply.github.com>
2022-06-06 20:07:51 +0530

    Update mongodb.service.js

--- app/services/mongodb.service.js
+++ app/services/mongodb.service.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.connect = () => {
     // connecting to mongo database
-    mongoose.connect('mongodb+srv://Admin:Vikrant@10@employeehunt.wg5fd.mongodb.net/employeehunt?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
+    mongoose.connect('mongodb+srv://Admin:Vikrant%4010@employeehunt.wg5fd.mongodb.net/test?authSource=admin&replicaSet=atlas-t4h35b-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', { useNewUrlParser: true, useFindAndModify: false ,useUnifiedTopology: true }, function (err) {
         if(err) {
             console.log(err);
         } else {
